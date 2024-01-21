00c5811e8fe27827f2462a360b27d57f71e50aaf
Gustavo Biava <gustavo.rbiava@hotmail.com>
2024-01-20 22:20:55 -0300

    User entity and resource

---
+++ src/main/java/free/biava/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package free.biava.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import free.biava.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1L, "Cristiano", "cristiano@gmail.com", "12345-6789", "12345");
+		return ResponseEntity.ok().body(user);
+	}
+
+}
