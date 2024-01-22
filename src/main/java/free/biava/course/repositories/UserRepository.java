6a88046440b0d2064a695585980c52fc3b93a1f9
Gustavo Biava <gustavo.rbiava@hotmail.com>
2024-01-21 21:05:59 -0300

    Service layer, component registration

--- src/main/java/free/biava/course/repositories/UserRepository.java
+++ src/main/java/free/biava/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package free.biava.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import free.biava.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
