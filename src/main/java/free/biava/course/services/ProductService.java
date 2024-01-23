f6f55ffe4666554d3d1fd6b0b4d7511d0de528b6
Gustavo Biava <gustavo.rbiava@hotmail.com>
2024-01-22 22:33:53 -0300

    Product

---
+++ src/main/java/free/biava/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package free.biava.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import free.biava.course.entities.Product;
+import free.biava.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id){
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
